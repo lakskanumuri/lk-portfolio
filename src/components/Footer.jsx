e2a4b4aa359107eaffb2130a8f54fc54b84f8d6f
lakskanumuri <lakskanumuri@gmail.com>
2026-05-26 22:00:18 -0400

    Remove extra character in footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer>
-      <p>Designed &amp; built by Lakshmi Kanumuri &nbsp;·&nbsp;</p>
+      <p>Designed &amp; built by Lakshmi Kanumuri &nbsp;</p>
     </footer>
   )
 }
